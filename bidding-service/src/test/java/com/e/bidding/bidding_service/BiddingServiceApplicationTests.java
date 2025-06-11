12b82b09d59aeabaf40fd44f0ebd71d8926966bd
M.A.H.K. Marasinghe <himanthamarasinghe@gmail.com>
2025-06-12 00:30:26 +0530

    Initial microservice project structure

---
+++ bidding-service/src/test/java/com/e/bidding/bidding_service/BiddingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e.bidding.bidding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ common-dtos/src/test/java/com/e/bidding/common_dtos/CommonDtosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e.bidding.common_dtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDtosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
